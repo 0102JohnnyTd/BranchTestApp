00784d22986757d6796b302be73c17ce84895320
0102johnny <rt2824.recruit.1022@gmail.com>
2024-02-18 19:51:55 +0900

    Initial commit

---
+++ BranchTestApp/BranchTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BranchTestAppApp.swift
+//  BranchTestApp
+//
+//  Created by Johnny Toda on 2024/02/18.
+//
+
+import SwiftUI
+
+@main
+struct BranchTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
